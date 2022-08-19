e469f2eb3fe02eb8bc63d55105ace8d1088ca3c2
satendra singh <satendra25@gmail.com>
2022-08-19 22:45:42 +0530

    Add project files.

---
+++ Controllers/TestHPAController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestHPA.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestHPAController : ControllerBase
+    {
+        // GET: <TestHPAController>
+        [HttpGet]
+        public string Get()
+        {
+            return new TestHPA().GetName();
+        }
+    }
+}
---
+++ TestHPA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestHPA
+{
+    public class TestHPA
+    {
+        public string Name { get; set; }
+
+        public string GetName()
+        {
+            for (int i = 1; i <= 10000; i++)
+            {
+                Name += "TestHPACalled " + i.ToString() + " Time"+Environment.NewLine;
+            }
+            return Name;
+        }
+    }
+}
